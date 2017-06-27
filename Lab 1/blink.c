ea9a877aeb0019363bbac092735ba9a4e988e327
haffizi <muab4039@colorado.edu>
2017-06-27 09:24:08 -0600

    lab 1 updated

--- Lab 1/blink.c
+++ Lab 1/blink.c
@@ -2,6 +2,16 @@
  * main.c
  */
 
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+
+#define GREEN GPIO_PIN_3
+#define BLUE GPIO_PIN_2
+#define RED GPIO_PIN_1
+
 extern int fib(int n);
 
 int main(void) {
@@ -11,7 +21,6 @@ int main(void) {
                        // need to add for loop for multiple digits
 	return 0;
 }
-
 
 // read the amount of digits, seperate and then blink them
 void mod(int a) {
@@ -36,19 +45,30 @@ int numPlaces (int n) {     //stolen code, used to find the amount of digits in 
 
 //blinking function 
 void blink (int n){
+
+    volatile uint32_t ui32Loop;     // Loop counter
+    int i;                          // Loop counter
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)){}
+
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, BLUE);       // Enable the blue LED
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, RED);        // Enable the red LED
+
     char pattern = convert_to_morse(n);
-    int dotDash = 0;
-    int pinLED = 0;
 
     for( int i = 0; i < 5; i++){             // 5 bits (0-4)
         dotDash = (pattern >> i) & 0x01;     // read a bit at a time
         if(dotDash == 1){
-        	pinLED = 1;
+            GPIOPinWrite(GPIO_PORTF_BASE, BLUE, BLUE);   // Turn off blue LED
+                    else GPIOPinWrite(GPIO_PORTF_BASE, RED, RED);               // Turn on red LED
         	for (int i = 0; i < 50000; i++){}    // long pause while LED on for a 1
         	pinLED = 0;
         }
         if else(dotDash == 0){
-        	pinLED = 1;
+            PIOPinWrite(GPIO_PORTF_BASE, BLUE, 0x0);    // Turn off blue LED
+                    else GPIOPinWrite(GPIO_PORTF_BASE, RED, 0x0);               // Turn off red LED
+
         	for (int i = 0; i < 20000; i++){}    // short pause while LED on for a 0
         	pinLED = 0;
         }
@@ -58,7 +78,6 @@ void blink (int n){
 		for(int i = 0; i < 50000; i++){}     // long pause while LED off between dot/dash
     }
 }
-
 
 //convert to Morse code
 char convert_to_morse (int n){
